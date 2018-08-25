2d352c8d9189e74f3191cd5df78b769dc3782927
Magic Len <len@magiclen.org>
2018-08-25 21:12:42 +0800

    add new APIs: encode_and_push_to_string, decode_and_push_to_vec

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@
 //! assert_eq!("SGVsbG8sIHdvcmxkIQ==", base64_url::unsafe_unescape("SGVsbG8sIHdvcmxkIQ"));
 //! ```
 //!
-//! Beside, in order to reduce the copy times of strings, you can also use `unsafe_escape_owned` and `unsafe_unescape_owned` associated functions to use the same memory space.
+//! Beside, in order to reduce the copy times of strings, you can also use `encode_and_push_to_string`, `decode_and_push_to_vec`, `unsafe_escape_owned` and `unsafe_unescape_owned` associated functions to use the same memory space.
 
 pub extern crate base64;
 
@@ -45,9 +45,72 @@ pub fn encode<T: ?Sized + AsRef<[u8]>>(input: &T) -> String {
     base64::encode_config(input, base64::URL_SAFE_NO_PAD)
 }
 
+/// Encode data to a Base64-URL string and directly store into a String instance by concatenating them. It is usually for generating a URL.
+pub fn encode_and_push_to_string<T: ?Sized + AsRef<[u8]>>(input: &T, output: String) -> String {
+    let bytes = input.as_ref();
+
+    let mut buffer = output.into_bytes();
+
+    let current_len = buffer.len();
+
+    let base64_len = ((bytes.len() + 4) / 4) * 4;
+
+    let min_capacity = current_len + base64_len;
+
+    let capacity = buffer.capacity();
+
+    if capacity < min_capacity {
+        buffer.reserve(min_capacity - capacity);
+    }
+
+    unsafe {
+        buffer.set_len(min_capacity);
+    }
+
+    let base64_len = base64::encode_config_slice(bytes, base64::URL_SAFE_NO_PAD, &mut buffer[current_len..min_capacity]);
+
+    unsafe {
+        buffer.set_len(current_len + base64_len);
+    }
+
+    String::from_utf8(buffer).unwrap()
+}
+
 /// Decode a Base64-URL string to data.
 pub fn decode<T: ?Sized + AsRef<[u8]>>(input: &T) -> Result<Vec<u8>, base64::DecodeError> {
     base64::decode_config(input, base64::URL_SAFE_NO_PAD)
+}
+
+/// Decode data to a Base64-URL string and directly store into a String instance by concatenating them.
+pub fn decode_and_push_to_vec<T: ?Sized + AsRef<[u8]>>(input: &T, mut output: Vec<u8>) -> Result<Vec<u8>, base64::DecodeError> {
+    let bytes = input.as_ref();
+
+    let current_len = output.len();
+
+    let original_max_len = {
+        let len = bytes.len();
+        (len + 3 / 4) * 3
+    };
+
+    let min_capacity = current_len + original_max_len;
+
+    let capacity = output.capacity();
+
+    if capacity < min_capacity {
+        output.reserve(min_capacity - capacity);
+    }
+
+    unsafe {
+        output.set_len(min_capacity);
+    }
+
+    let original_len = base64::decode_config_slice(bytes, base64::URL_SAFE_NO_PAD, &mut output[current_len..min_capacity])?;
+
+    unsafe {
+        output.set_len(current_len + original_len);
+    }
+
+    Ok(output)
 }
 
 /// Escape a Base64 string to a Base64-URL string. It is unsafe because the conversion is not concerning with Base64 decoding. You need to make sure the input string is a correct Base64 string by yourself.
@@ -95,7 +158,7 @@ pub fn unsafe_escape_owned(base64_str: String) -> String {
 pub fn unsafe_unescape(base64_str: &str) -> String {
     let len = base64_str.len();
 
-    let mut result = Vec::with_capacity(((len + 3) / 4) * 4);
+    let mut result = Vec::with_capacity(((len + 4) / 4) * 4);
 
     for &n in base64_str.as_bytes() {
         if n == 45 {
@@ -151,8 +214,22 @@ mod tests {
     }
 
     #[test]
+    fn test_encode_and_push_to_string() {
+        let url = "https://magiclen.org/".to_string();
+
+        assert_eq!("https://magiclen.org/YXJ0aWNsZXM", encode_and_push_to_string("articles", url));
+    }
+
+    #[test]
     fn test_decode() {
         assert_eq!("https://magiclen.org".as_bytes().to_vec(), decode("aHR0cHM6Ly9tYWdpY2xlbi5vcmc").unwrap());
+    }
+
+    #[test]
+    fn test_decode_and_push_to_vec() {
+        let url = "https://magiclen.org/".as_bytes().to_vec();
+
+        assert_eq!("https://magiclen.org/articles".as_bytes().to_vec(), decode_and_push_to_vec("YXJ0aWNsZXM", url).unwrap());
     }
 
     #[test]
