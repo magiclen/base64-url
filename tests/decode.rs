a43befc30b648663ba09204aeb5d54d90565ef78
Magic Len <len@magiclen.org>
2019-09-17 01:45:58 +0800

    imporve code

--- tests/decode.rs
+++ tests/decode.rs
@@ -3,17 +3,17 @@ extern crate base64_url;
 #[test]
 fn decode() {
     assert_eq!(
-        "https://magiclen.org".as_bytes().to_vec(),
+        b"https://magiclen.org".to_vec(),
         base64_url::decode("aHR0cHM6Ly9tYWdpY2xlbi5vcmc").unwrap()
     );
 }
 
 #[test]
 fn decode_and_push_to_vec() {
-    let url = "https://magiclen.org/".as_bytes().to_vec();
+    let url = b"https://magiclen.org/".to_vec();
 
     assert_eq!(
-        "https://magiclen.org/articles".as_bytes().to_vec(),
+        b"https://magiclen.org/articles".to_vec(),
         base64_url::decode_and_push_to_vec("YXJ0aWNsZXM", url).unwrap()
     );
 }
--- tests/unescape.rs
+++ tests/unescape.rs
@@ -5,7 +5,7 @@ use base64_url::base64;
 #[test]
 fn unsafe_unescape() {
     assert_eq!(
-        "https://magiclen.org".as_bytes().to_vec(),
+        b"https://magiclen.org".to_vec(),
         base64::decode(&base64_url::unsafe_unescape("aHR0cHM6Ly9tYWdpY2xlbi5vcmc")).unwrap()
     );
 }
